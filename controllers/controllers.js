d0d00a697ea2f33ecb35709933d4cecc9fc6a67e
Néstor Iriondo <nestoririondo@gmail.com>
2024-02-02 14:27:48 +0100

    Level 3

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,19 +1,22 @@
 import path from "path";
 import fs from "fs/promises";
+import pool from "../db/pool.js";
 
 export const postProfilePic = async (req, res) => {
   if (!req.file) return res.sendStatus(400);
   const oldPath = path.resolve(req.file.path);
   const newPath = path.resolve(req.file.destination, req.file.originalname);
-
+  const encodedFilename = encodeURIComponent(req.file.originalname);
   try {
     await fs.rename(oldPath, newPath);
+    const query = `INSERT INTO pictures (name, path) VALUES ($1, $2) RETURNING *`;
+    const values = [req.file.originalname, `/uploads/${encodedFilename}`];
+    const { rows } = await pool.query(query, values);
+    console.log(rows[0]);
     res
       .status(201)
       .send(
-        "<div><h2>Here's the picture:</h2><img src='/uploads/" +
-          req.file.originalname +
-          "'/></div>"
+        `<div><h2>Here's the picture:</h2><img src='/uploads/${encodedFilename}'/></div>`
       );
   } catch (error) {
     console.error(error);
@@ -21,21 +24,51 @@ export const postProfilePic = async (req, res) => {
   }
 };
 
-export const postCatPics = (req, res) => {
-  if (!req.files) return res.sendStatus(400);
-  let images = [];
-  req.files.map(async (file) => {
-    const oldPath = path.resolve(file.path);
-    const newPath = path.resolve(file.destination, file.originalname);
-    try {
-      await fs.rename(oldPath, newPath);
-      images.push("<img src='/uploads/" + file.originalname + "'/>");
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
-    }
-  });
+export const postCatPics = async (req, res) => {
+  if (!req.files || req.files.length === 0) return res.sendStatus(400);
+  const images = [];
+  try {
+    await Promise.all(
+      req.files.map(async (file) => {
+        const encodedFilename = encodeURIComponent(file.originalname);
+        const oldPath = path.resolve(file.path);
+        const newPath = path.resolve(file.destination, file.originalname);
+        await fs.rename(oldPath, newPath);
+        images.push(
+          `<img style="max-width: 100%" src='/uploads/${encodedFilename}'/>`
+        );
+        const query = `INSERT INTO pictures (name, path) VALUES ($1, $2) RETURNING *`;
+        const values = [file.originalname, `/uploads/${encodedFilename}`];
+        const { rows } = await pool.query(query, values);
+        console.log(rows[0]);
+      })
+    );
+    res
+      .status(201)
+      .send(`<div><h2>Here are the pictures:</h2>${images.join("")}</div>`);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+};
+
+export const getPics = async (req, res) => {
+  const pics = [];
+  try {
+    const query = "SELECT * FROM pictures";
+    const { rows } = await pool.query(query);
+    rows.forEach((row) =>
+      pics.push(
+        `<img style="max-width: 100%" src=${row.path} alt=${row.name}/>`
+      )
+    );
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
   res
-    .status(201)
-    .send("<div><h2>Here are the pictures:</h2>" + images.join("") + "</div>");
+    .status(200)
+    .send(
+      `<div><h2>Here are all the uploaded pictures:</h2>${pics.join("")}</div>`
+    );
 };
--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { uploadPic } from "../utils/multer.js";
-import { postProfilePic, postCatPics } from "../controllers/controllers.js";
+import { postProfilePic, postCatPics, getPics } from "../controllers/controllers.js";
 
 const router = express.Router();
 
 router.post("/upload-profile-pic", uploadPic.single('profile_pic'), postProfilePic);
 router.post("/upload-cat-pics", uploadPic.array('cat_pics'), postCatPics);
+router.get("/get-pics", getPics);
 
 export default router;
+
+
+// Insert a link into your HTML that will point to /get-pics & create GET a route handler for /get-pics
+// This route handler should get all the pictures previously uploaded in the database and return them as a list of links to the user. The user should be able to click on a link, and view the picture
